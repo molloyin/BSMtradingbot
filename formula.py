aea4a03fe933678787dd150724cea12291a05e81
Matt (Jeff) Molloy <geothermalpathways@icloud.com>
2023-01-27 17:00:00 +1300

    modified several files

--- formula.py
+++ formula.py
@@ -1,9 +1,20 @@
-import math
+from math import sqrt, e, log
+from scipy.stats import norm
 
 
-def call_option(current, strike, risk_free_interest, expire_t, cdf1, cdf2):
-    return current * cdf1 - strike * math.e(-risk_free_interest * expire_t) * cdf2
+def call_option(current, strike, risk_free_interest, t, sigma):
+    d1 = _d1(current, strike, risk_free_interest, t, sigma) 
+    d2 = _d2(d1, sigma, t)
+    return current * norm.cdf(d1) - strike * e(-risk_free_interest * t) * norm.cdf(d2)
 
 
-def put_option(current, strike, risk_free_interest, expire_t, cdf1, cdf2):
-    return strike * math.e(-risk_free_interest * expire_t) * cdf2 - current * cdf1
+def put_option(current, strike, risk_free_interest, t, sigma):
+    d1 = _d1(current, strike, risk_free_interest, t, sigma) 
+    d2 = _d2(d1, sigma, t)
+    return strike * e(-risk_free_interest * t) * norm.cdf(d2) - current * norm.cdf(d1)
+
+def _d1(S, K, r, t, sigma):
+    return (log(S / K) + (r + sigma ** 2 / 2) * t) / (sigma * sqrt(t))
+
+def _d2(d1, sigma, t):
+    return d1 - sigma * sqrt(t)
---
+++ model.py
@@ -0,0 +1,2 @@
+import formula
+import webscrubber
