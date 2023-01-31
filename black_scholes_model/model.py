e928e9a25a7f5a58337490d08ea76cda2e165232
Matt (Jeff) Molloy <geothermalpathways@icloud.com>
2023-02-01 12:04:59 +1300

    fixed put pricing bug

--- black_scholes_model/model.py
+++ black_scholes_model/model.py
@@ -1,10 +1,13 @@
 from formula import call_option, put_option
 from webscrubber import stock, s, strike, contract_period, sigma, r
 
+print("stock: ", stock, " current price: ", s,
+      " risk-free rate: ", r, " volatility: ", sigma, " strike", strike)
+
 print("For:", stock, " with strike price: ", strike,
-      " and contract period: ", contract_period, " years")
+      " and contract period: ", "{:.2f}".format(contract_period*12), " months")
 call = call_option(s, strike, r, contract_period, sigma)
 put = put_option(s, strike, r, contract_period, sigma)
 
 print("Call option price: ", call)
-print("Put option price: ", call)
+print("Put option price: ", put)
